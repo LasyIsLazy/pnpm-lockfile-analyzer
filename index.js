11dcbd934c8dcaad0b127356963dd8cc534bc2be
lasy <1255352506@qq.com>
2025-01-23 15:49:08 +0800

    add env

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+#!/usr/bin/env node
 import { parse } from './parse.js';
 import { createServer } from 'vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import manifest from './package.json' assert { type: 'json' };
 
 const [folderPath] = process.argv.slice(2);
+if (folderPath === '--version') {
+  console.log(manifest.version);
+  process.exit(0);
+}
 if (!folderPath) {
   throw new Error('You should pass an argument as folder path');
 }
